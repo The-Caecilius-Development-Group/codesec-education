284e6c04467ecd864ba24a771fac6bc69b5c3a42
lolgeny <joel@swedensky.com>
2022-02-18 21:01:40 +0000

    stylesheet

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
+#![allow(non_snake_case)]
+#![deny(unsafe_code)]
+
 use dioxus::prelude::*;
 
-fn app(cx: Scope) -> Element {
+
+fn HomePage(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {"Hello world!"}
+        div {
+            class: "center-div",
+            h1 {"CodeSec"}
+        }
+    })
+}
+
+fn App(cx: Scope) -> Element {
+    rsx! (cx, div {
+        style {[include_str!("style.css")]},
+        link {
+            rel: "preconnect", href: "https://fonts.googleapis.com"
+        },
+        link {
+            rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "true"
+        },
+        link {
+            rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap"
+        }
+        HomePage {
+
+        }
     })
 }
 
 fn main() {
-    dioxus::desktop::launch(app);
+    dioxus::desktop::launch(App);
 }
