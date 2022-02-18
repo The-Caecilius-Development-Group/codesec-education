d214945f33cc2e32da170ecf47e20f8adf5758d2
The Average Avocado <rayan.nadeem@outlook.com>
2022-02-18 22:38:40 +0000

    added flashcard input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
 use dioxus::prelude::*;
+mod note_input;
 
-fn app(cx: Scope) -> Element {
+fn HomePage(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {"Hello world!"}
+        div {
+            class: "center-div",
+            h1 {"CodeSec"}
+        }
     })
 }
 
+fn App(cx: Scope) -> Element {
+    rsx! (cx, div {
+        style {[include_str!("style.css")]},
+        link {
+            rel: "preconnect", href: "https://fonts.googleapis.com"
+        },
+        link {
+            rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "true"
+        },
+        link {
+            rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap"
+        },
+        link{
+            rel: "styelsheet", href: "https://fonts.googleapis.com/css2?family=Orbitron&display=swap"
+        }
+        HomePage {
+
+        }
+        note_input::input_flashcards {
+
+        }
+    })
+}
+
+
 fn main() {
-    dioxus::desktop::launch(app);
+    dioxus::desktop::launch(App);
 }
